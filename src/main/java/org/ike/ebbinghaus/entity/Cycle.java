43aee1133947ca199cb068272bee93192d4f0fb6
chenyuan <chenyuan@cdcosmistar.com>
2019-02-12 14:24:50 +0800

    自动格式化时间

--- src/main/java/org/ike/ebbinghaus/entity/Cycle.java
+++ src/main/java/org/ike/ebbinghaus/entity/Cycle.java
@@ -1,6 +1,8 @@
 package org.ike.ebbinghaus.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -11,10 +13,14 @@ public class Cycle {
 
     private String createdBy;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd",timezone="GMT+8")
     private Date createdTime;
 
     private String updatedBy;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd",timezone="GMT+8")
     private Date updatedTime;
 
     private Integer unit;
--- src/main/java/org/ike/ebbinghaus/entity/Resource.java
+++ src/main/java/org/ike/ebbinghaus/entity/Resource.java
@@ -1,6 +1,8 @@
 package org.ike.ebbinghaus.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -10,16 +12,24 @@ public class Resource {
 
     private String createdBy;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd",timezone="GMT+8")
     private Date createdTime;
 
     private String updatedBy;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd",timezone="GMT+8")
     private Date updatedTime;
 
     private String content;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd",timezone="GMT+8")
     private Date firstTime;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd",timezone="GMT+8")
     private Date lastTime;
 
     private String memory;
