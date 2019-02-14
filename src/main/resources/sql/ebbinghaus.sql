49d868c105944513ddce1cf01bfb90818bdd35d8
chenyuan <chenyuan@cdcosmistar.com>
2019-02-14 09:28:16 +0800

    整合sql源文件

--- src/main/resources/sql/ebbinghaus.sql
+++ src/main/resources/sql/ebbinghaus.sql
@@ -10,10 +10,26 @@ Target Server Type    : MYSQL
 Target Server Version : 50534
 File Encoding         : 65001
 
-Date: 2019-02-13 14:57:46
+Date: 2019-02-14 09:26:25
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for cycle
+-- ----------------------------
+DROP TABLE IF EXISTS `cycle`;
+CREATE TABLE `cycle` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `CREATED_BY` varchar(32) DEFAULT NULL COMMENT '创建人',
+  `CREATED_TIME` datetime DEFAULT NULL COMMENT '创建时间',
+  `UPDATED_BY` varchar(32) DEFAULT NULL COMMENT '更新人',
+  `UPDATED_TIME` datetime DEFAULT NULL COMMENT '更新时间',
+  `UNIT` int(11) DEFAULT NULL COMMENT '单位',
+  `INCREMENT` int(11) DEFAULT NULL COMMENT '增量',
+  `CAPACITY` decimal(64,5) DEFAULT NULL COMMENT '记忆占比',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COMMENT='周期 ';
 
 -- ----------------------------
 -- Table structure for resource
