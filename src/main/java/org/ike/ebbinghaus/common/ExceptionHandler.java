175d8307a9e0a32be04f650efe87ffd93944720f
chenyuan <chenyuan@cdcosmistar.com>
2019-02-15 18:05:38 +0800

    (处理)捕获全局异常

---
+++ src/main/java/org/ike/ebbinghaus/common/ExceptionHandler.java
@@ -0,0 +1,14 @@
+package org.ike.ebbinghaus.common;
+
+/**
+ * @Author: ike
+ * @Date: 2019-02-15 17:38
+ */
+public class ExceptionHandler extends  Exception {
+
+    ExceptionHandler() {
+
+    }
+
+
+}
